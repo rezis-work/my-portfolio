a09bbb7c811cf2620579eca70e380a583f2a1d20
Rezi Karanadze <karanadzerevazi@gmail.com>
2024-05-19 12:27:48 +0400

    implement projects section

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -97,7 +97,7 @@ export const BentoGridItem = ({
 
         {id === 6 && (
           <BackgroundGradientAnimation>
-            <div className=" absolute z-50 flex items-center justify-center text-white font-bold"></div>
+            {/* <div className=" absolute z-50 flex items-center justify-center text-white font-bold"></div> */}
           </BackgroundGradientAnimation>
         )}
 
