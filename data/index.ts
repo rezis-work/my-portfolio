9dfb29c9a9ce0e951c12ad2910df7b8cf21c1613
Rezi Karanadze <karanadzerevazi@gmail.com>
2024-05-19 14:51:53 +0400

    implement work experience section

--- data/index.ts
+++ data/index.ts
@@ -192,22 +192,22 @@ export const workExperience = [
   },
   {
     id: 2,
-    title: "Mobile App Dev - JSM Tech",
-    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    title: "Front End junior developer",
+    desc: "Designed and developed web applications and games with JavaScript",
     className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/exp2.svg",
   },
   {
     id: 3,
-    title: "Freelance App Dev Project",
-    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    title: "Front End middle developer",
+    desc: "Working on Casino online games design, pixel perfect, developing game functionality",
     className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/exp3.svg",
   },
   {
     id: 4,
-    title: "Lead Frontend Developer",
-    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    title: "Full Stack Software Engineer",
+    desc: "Developed and maintained user-facing features using modern frontend technologies, Also work with students and juniors.",
     className: "md:col-span-2",
     thumbnail: "/exp4.svg",
   },
